e48af6baabcb29a0b333419dc07785282250c9fe
Yu JIANG <1025368039@qq.com>
2024-06-23 19:47:15 +0800

    path_select

--- src/saver.rs
+++ src/saver.rs
@@ -6,23 +6,20 @@ use serde::{Serialize, Deserialize};
 
 use crate::data::{TodoState, TodoItem};
 
+// const CONFIG_PATH: &str = "D:/Code/Rust_Pratice/test/MyTodo.json";
+
 pub struct Saver;
 
 impl Widget<TodoState> for Saver {
-    fn event(&mut self, _ctx: &mut druid::EventCtx, _event: &druid::Event,_data: &mut TodoState, _env: &druid::Env) {
-    }
+    fn event(&mut self, _ctx: &mut druid::EventCtx, _event: &druid::Event, _data: &mut TodoState, _env: &druid::Env) {}
 
-    fn lifecycle(&mut self, _ctx: &mut druid::LifeCycleCtx, _event: &druid::LifeCycle, _data: &TodoState, _env: &druid::Env) {
-    }
+    fn lifecycle(&mut self, _ctx: &mut druid::LifeCycleCtx, _event: &druid::LifeCycle, _data: &TodoState, _env: &druid::Env) {}
 
     fn update(&mut self, _ctx: &mut druid::UpdateCtx, old_data: &TodoState, data: &TodoState, _env: &druid::Env) {
         if data.todos != old_data.todos {
-            if let Some(base_dirs) = BaseDirs::new() {
-                let config = format!("{}/{}", base_dirs.config_dir().to_str().unwrap(), "MyTodo.json");
-                let config_path = Path::new(&config);
-                let tasks = TaskData {tasks: data.todos.clone().into_iter().collect() };
-                fs::write(config_path, serde_json::to_string(&tasks).unwrap()).expect("Config path does not fully exist");
-            }
+            let config_path = Path::new("MyTodo.json");
+            let tasks = TaskData { tasks: data.todos.clone().into_iter().collect() };
+            fs::write(config_path, serde_json::to_string(&tasks).unwrap()).expect("Config path does not fully exist");
         }
     }
 
@@ -30,8 +27,7 @@ impl Widget<TodoState> for Saver {
         druid::Size { width: 0., height: 0. }
     }
 
-    fn paint(&mut self, _ctx: &mut druid::PaintCtx, _data: &TodoState, _env: &druid::Env) {
-    }
+    fn paint(&mut self, _ctx: &mut druid::PaintCtx, _data: &TodoState, _env: &druid::Env) {}
 }
 
 #[derive(Serialize, Deserialize)]
@@ -40,21 +36,16 @@ pub struct TaskData {
 }
 
 pub fn read_stored() -> TaskData {
-    if let Some(base_dirs) = BaseDirs::new() {
-        let config = format!("{}/{}", base_dirs.config_dir().to_str().unwrap(), "MyTodo.json");
-        let config_path = Path::new(&config);
-        let data = match fs::read_to_string(config_path) {
-            Ok(a) => a,
-            Err(_) => return TaskData {tasks: Vec::new()},
-        };
-        match serde_json::from_str(&data) {
-            Ok(a) => a,
-            Err(e) => {
-                eprintln!("The save data is corrupted or no longer in the format it should be in\nError {}", e);
-                return TaskData { tasks: Vec::new() };
-            },
-        }
-    } else {
-        return TaskData { tasks: Vec::new() };
+    let config_path = Path::new("MyTodo.json");
+    let data = match fs::read_to_string(config_path) {
+        Ok(a) => a,
+        Err(_) => return TaskData { tasks: Vec::new() },
+    };
+    match serde_json::from_str(&data) {
+        Ok(a) => a,
+        Err(e) => {
+            eprintln!("The save data is corrupted or no longer in the format it should be in\nError {}", e);
+            return TaskData { tasks: Vec::new() };
+        },
     }
 }
